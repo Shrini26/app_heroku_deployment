740affb3655770d9f90ac8a5cd27a8bc10db1557
Shrinivas <shrinivasshendye@gmail.com>
2021-06-20 11:58:42 +0530

    added method for favicon

--- app1.py
+++ app1.py
@@ -6,5 +6,9 @@ app1=Flask(__name__)
 def index():
     return "<h1> this is a flask application </h1>"
 
+@app1.route('/favicon.ico',methods=['GET'])
+def favicon():
+    return "<h1>Hello</h1>"
+
 if __name__=="__main__":
     app1.run()
